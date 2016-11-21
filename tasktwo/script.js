41e2d2074c44dcc06e55dfa6305b39ad90b722bd
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-11-21 14:09:56 +0200

    changed JS function to take parameters

--- tasktwo/script.js
+++ tasktwo/script.js
@@ -2,13 +2,13 @@
  * Created by Peter Nedelchev <peter.krasimirov@gmail.com>.
  */
 
-function loadXml() {
+function loadXml(method, file, elementId) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("source").innerHTML = this.responseText;
+            document.getElementById(elementId).innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "employee.json", true);
+    xhttp.open(method, file, true);
     xhttp.send();
 }
