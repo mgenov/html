11be252becd7fa6b21240359cb6d6c14bb7bfb85
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-11-23 10:27:09 +0200

    All names changed to camilCase and methods renamed to concrete names

--- taskthree/script.js
+++ taskthree/script.js
@@ -2,26 +2,26 @@
  * Created by Peter Nedelchev <peter.krasimirov@gmail.com>.
  */
 
-function validator(form) {
-    validate(/^[a-zA-z]{1,15}/, form.firstname.value, 'firstname_alert', 'Bad name');
-    validate(/^[a-zA-z]{1,15}/, form.lastname.value, 'lastname_alert', 'Bad name');
-    validate(/\d{10}/, form.egn.value, 'egn_alert', 'Egn is exact 10 digits');
+function validateUserRegistrationForm(form) {
+    validateFormField(/^[a-zA-z]{1,15}/, form.firstName.value, 'firstNameAlert', 'Bad name');
+    validateFormField(/^[a-zA-z]{1,15}/, form.lastname.value, 'lastNameAlert', 'Bad name');
+    validateFormField(/\d{10}/, form.egn.value, 'egn_alert', 'Egn is exact 10 digits');
     if(form.age.value < 18 || 118 > form.age.value) {
-        document.getElementById("age_alert").innerHTML = "Incorrect age";
+        document.getElementById("ageAlert").innerHTML = "Incorrect age";
         return;
     }
-    validate(/^[a-zA-z0-9]{1,100}/, form.address.value, 'address_alert', 'Address ');
-    validate(/^([a-zA-z0-9]{6,18})$/, form.password.value, 'password_alert', 'Only letters and digits');
-    if (password != confirmedPass) {
-        document.getElementById("passconf_alert").innerHTML = "Password doesn't match";
+    validateFormField(/^[a-zA-z0-9]{1,100}/, form.address.value, 'addressAlert', 'Address ');
+    validateFormField(/^([a-zA-z0-9]{6,18})$/, form.password.value, 'passwordAlert', 'Only letters and digits');
+    if (form.password.value != form.passConfirm.value) {
+        document.getElementById("passConfAlert").innerHTML = "Password doesn't match";
         return;
     }
 }
 
-function validate(regex, value, errorId, message) {
-    if (!regex.test(value)) {
-        document.getElementById(errorId).innerHTML = message;
+function validateFormField(regex, fieldValue, errorFieldId, errorMessage) {
+    if (!regex.test(fieldValue)) {
+        document.getElementById(errorFieldId).innerHTML = errorMessage;
         return;
     }
-    document.getElementById(errorId).innerHTML = "";
+    document.getElementById(errorFieldId).innerHTML = "";
 }
