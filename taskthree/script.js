c969e207f5dc04cd4b43e34fedca4d61cfdb47fd
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-11-23 10:35:47 +0200

    Age validation using RegEx and clear confirm error message if passwords match

--- taskthree/script.js
+++ taskthree/script.js
@@ -6,16 +6,14 @@ function validateUserRegistrationForm(form) {
     validateFormField(/^[a-zA-z]{1,15}/, form.firstName.value, 'firstNameAlert', 'Bad name');
     validateFormField(/^[a-zA-z]{1,15}/, form.lastname.value, 'lastNameAlert', 'Bad name');
     validateFormField(/\d{10}/, form.egn.value, 'egn_alert', 'Egn is exact 10 digits');
-    if(form.age.value < 18 || 118 > form.age.value) {
-        document.getElementById("ageAlert").innerHTML = "Incorrect age";
-        return;
-    }
+    validateFormField(/^(1[89])$|^([2-9][0-9])$|^(10[0-9])$|^(11[0-8])$/, form.age.value, 'ageAlert', 'Allowed from 18 to 118');
     validateFormField(/^[a-zA-z0-9]{1,100}/, form.address.value, 'addressAlert', 'Address ');
     validateFormField(/^([a-zA-z0-9]{6,18})$/, form.password.value, 'passwordAlert', 'Only letters and digits');
     if (form.password.value != form.passConfirm.value) {
         document.getElementById("passConfAlert").innerHTML = "Password doesn't match";
         return;
     }
+    document.getElementById("passConfAlert").innerHTML = ""
 }
 
 function validateFormField(regex, fieldValue, errorFieldId, errorMessage) {
