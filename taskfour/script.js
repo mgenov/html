37a68cc1d5ffd9f3ca7da3f8dfddc266084897b3
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-11-24 15:51:39 +0200

    Calculator with validated input

--- taskfour/script.js
+++ taskfour/script.js
@@ -1,21 +1,45 @@
 /**
  * Created by Peter Nedelchev <peter.krasimirov@gmail.com>.
  */
-function calculate(element_id) {
-    var expression = document.getElementById(element_id).value;
-    document.getElementById(element_id).value = eval(expression);
+function calculateExpression(elementId) {
+    var expression = document.getElementById(elementId).value;
+    if(expression.length != 0) {
+        document.getElementById(elementId).value = eval(expression);
+        return;
+    }
 }
 
-function append(element_id, value) {
-    document.getElementById(element_id).value += value;
+function cleanDisplay(elementId) {
+    document.getElementById(elementId).value = "";
 }
 
-function clean(element_id) {
-    document.getElementById(element_id).value = "";
+function clearLastChar(elementId) {
+    var value = document.getElementById(elementId).value;
+    document.getElementById(elementId).value = value.slice(0, value.length - 1);
 }
 
-function clearLast(element_id) {
-    var value = document.getElementById(element_id).value;
-    document.getElementById(element_id).value = value.slice(0, value.length - 1);
+
+function appendDigit(elementId, digit) {
+    document.getElementById(elementId).value += digit;
+}
+
+function appendOperand(elementId, operand) {
+    var currentExpression = document.getElementById(elementId).value;
+    var lastChar = currentExpression.slice(-1);
+    if(/(\.|-|\+|\*|\/)/.test(lastChar) || currentExpression.length == 0) {
+        document.getElementById(elementId).value = currentExpression;
+        return;
+    }
+    document.getElementById(elementId).value += operand;
+}
+
+function appendDot(elementId) {
+    var currentExpression = document.getElementById(elementId).value;
+    var tokens = currentExpression.split(/[-+*/]/);
+    if(/(?=[.])/.test(tokens[tokens.length -1]) || currentExpression.length == 0) {
+        document.getElementById(elementId).value = currentExpression;
+        return;
+    }
+    document.getElementById(elementId).value += ".";
 }
 
