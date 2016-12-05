5f5a660f58c7417358c9b12aede28436bb3bb7cd
Miroslav Genov <mgenov@gmail.com>
2016-12-05 20:15:18 +0200

    js: refactoring fraction validation page
    The page that validates fractions is now refactored and is having tests (test.html)
    
    isFraction function is extracted from the validate() function to provide
    better design and easy testing.

--- fractionvalidator/fraction.js
+++ fractionvalidator/fraction.js
@@ -1,14 +1,22 @@
-function validate() {
-    var fraction = document.getElementById("input").value;
-    var numbers = /^[0-9]{1,10}[.,]{1}([0-9]{1,5})$/;
+function isFraction(value) {
+  var numbers = /^[0-9]{1,10}[.,]{1}([0-9]{1,5})$/;
 
-    if ((fraction.indexOf(".") == -1) && (fraction.indexOf(",") == -1)) {
-        fraction = fraction + ".0";
-    }
+  if ((value.indexOf(".") == -1) && (value.indexOf(",") == -1)) {
+    value = value + ".0";
+  }
 
-    if (fraction.match(numbers)) {
-        document.getElementById("result").value = "True";
-    } else {
-        document.getElementById("result").value = "False";
-    }
+  if (value.match(numbers)) {
+    return true;
+  }
+
+  return false
+}
+
+function checkFractionValue() {
+  var fraction = document.getElementById("input").value;
+  if (isFraction(fraction)) {
+    document.getElementById("result").value = "True";
+  } else {
+    document.getElementById("result").value = "False";
+  }
 }
